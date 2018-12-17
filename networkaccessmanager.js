3c9fc3cb0ccb67b94cf6a8d511f05cf8fc771df6
Sandro S. Andrade <sandroandrade@kde.org>
2018-12-17 16:45:57 -0300

    Factor our timeout handling

--- networkaccessmanager.js
+++ networkaccessmanager.js
@@ -10,32 +10,28 @@ function reset() {
     errorText.text = ""
 }
 
+function timeout() {
+    httpRequest.abort()
+    timer.stop()
+    timer.destroy()
+    busyIndicator.running = false
+    errorText.text = "erro de timeout\nsem conexão ou servidor SEI indisponível"
+}
+
 function get(url) {
     httpRequest.open("GET", url);
     httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     timer = Qt.createQmlObject("import QtQuick 2.12; Timer { interval: 20000; repeat: false; running: true }", Qt.application, "timeoutTimer");
-    timer.triggered.connect(function() {
-        httpRequest.abort()
-        timer.stop()
-        timer.destroy()
-        busyIndicator.running = false
-        errorText.text = "erro de timeout\nsem conexão ou servidor SEI indisponível";
-    })
+    timer.triggered.connect(timeout)
     busyIndicator.running = true
-    httpRequest.send();
+    httpRequest.send()
 }
 
 function post(url, params) {
     httpRequest.open("POST", url);
     httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     timer = Qt.createQmlObject("import QtQuick 2.12; Timer { interval: 20000; repeat: false; running: true }", Qt.application, "timeoutTimer");
-    timer.triggered.connect(function() {
-        httpRequest.abort()
-        timer.stop()
-        timer.destroy()
-        busyIndicator.running = false
-        errorText.text = "erro de timeout\nsem conexão ou servidor SEI indisponível";
-    })
+    timer.triggered.connect(timeout)
     busyIndicator.running = true
-    httpRequest.send(params);
+    httpRequest.send(params)
 }
